d27eaa1c70f3c44da0d3dc521f669de4f5c7d175
Jörn Dinkla <joern@dinkla.net>
2023-11-10 23:35:15 +0100

    refactor test Fixture.kt

---
+++ src/test/kotlin/net/dinkla/kpnk/Fixture.kt
@@ -0,0 +1,28 @@
+package net.dinkla.kpnk
+
+private const val KOTLIN_EXAMPLE_FILE = "src/test/resources/example/HelloWorld.kt"
+
+internal val tree = fromFile(KOTLIN_EXAMPLE_FILE)
+
+internal val expectedImports = listOf(
+    Import(FullyQualifiedName("java.lang.Boolean.FALSE")),
+    Import(FullyQualifiedName("java.lang.Boolean.TRUE")),
+)
+
+internal val function1 = FunctionSignature(
+    "topLevelFunction",
+    "String",
+    listOf(Parameter("n", "Int"), Parameter("hw", "HelloWorld")),
+)
+
+internal val function2 = FunctionSignature(
+    "main",
+    "Unit",
+    listOf(),
+)
+
+internal val class1 = ClassSignature(
+    "HelloWorld",
+    listOf(Parameter("many", "Int")),
+    listOf(FunctionSignature("toString", "String", listOf())),
+)
--- src/test/kotlin/net/dinkla/kpnk/TraverseTest.kt
+++ src/test/kotlin/net/dinkla/kpnk/TraverseTest.kt
@@ -4,33 +4,6 @@ import io.kotest.core.spec.style.StringSpec
 import io.kotest.matchers.collections.shouldContainExactly
 import io.kotest.matchers.shouldBe
 
-private const val KOTLIN_EXAMPLE_FILE = "src/test/resources/example/HelloWorld.kt"
-
-private val tree = fromFile(KOTLIN_EXAMPLE_FILE)
-
-private val expectedImports = listOf(
-    Import(FullyQualifiedName("java.lang.Boolean.FALSE")),
-    Import(FullyQualifiedName("java.lang.Boolean.TRUE")),
-)
-
-private val function1 = FunctionSignature(
-    "topLevelFunction",
-    "String",
-    listOf(Parameter("n", "Int"), Parameter("hw", "HelloWorld")),
-)
-
-private val function2 = FunctionSignature(
-    "main",
-    "Unit",
-    listOf(),
-)
-
-private val class1 = ClassSignature(
-    "HelloWorld",
-    listOf(Parameter("many", "Int")),
-    listOf(FunctionSignature("toString", "String", listOf())),
-)
-
 class TraverseTest : StringSpec({
     "traverse should return all information" {
         val file = traverse(tree)
