ad2a548d70c041accad1a30892187ce642c1a504
Jörn Dinkla <joern@dinkla.net>
2023-11-10 21:31:15 +0100

    improve toString methods

--- src/main/kotlin/net/dinkla/kpnk/Elements.kt
+++ src/main/kotlin/net/dinkla/kpnk/Elements.kt
@@ -6,17 +6,26 @@ data class File(
 )
 
 @JvmInline
-value class FullyQualifiedName(val name: String) {
+value class FullyQualifiedName(private val name: String) {
     override fun toString(): String = name
 }
 
-data class Import(val fullyQualifiedName: FullyQualifiedName) {
-    override fun toString(): String = "Import($fullyQualifiedName)"
+data class Import(val name: FullyQualifiedName) {
+    override fun toString(): String = "import $name"
 }
 
-data class Parameter(val name: String, val type: String)
+data class Parameter(val name: String, val type: String) {
+    override fun toString(): String = "$name: $type"
+}
 
-data class FunctionSignature(val name: String, val returnType: String, val parameters: List<Parameter>)
+data class FunctionSignature(val name: String, val returnType: String, val parameters: List<Parameter>) {
+    override fun toString(): String {
+        return "fun $name(${prettyParameters}): $returnType"
+    }
+
+    private val prettyParameters: String
+        get() = if (parameters.isEmpty()) "" else parameters.joinToString(", ") { it.toString() }
+}
 
 data class ObjectSignature(val name: String)
 
