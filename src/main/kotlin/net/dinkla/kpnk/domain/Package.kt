3eae9c5afb0ac3cb5f37b1b5504eb7b37226ced0
Jörn Dinkla <jdinkla@thoughtworks.com>
2024-02-03 12:00:49 +0100

    make Package serializable

--- src/main/kotlin/net/dinkla/kpnk/domain/Package.kt
+++ src/main/kotlin/net/dinkla/kpnk/domain/Package.kt
@@ -1,5 +1,8 @@
 package net.dinkla.kpnk.domain
 
+import kotlinx.serialization.Serializable
+
+@Serializable
 data class Package(val name: PackageName, val files: List<AnalysedFile>) {
     override fun toString(): String {
         return "${name.name}: ${files.joinToString(", ") { it.packageName() }}"
