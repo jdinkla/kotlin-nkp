751d494189a619c9729baeb658a18879e1dafe8d
Jörn Dinkla <jdinkla@thoughtworks.com>
2025-04-09 16:57:38 +0200

    Upgrade Gradle to 8.13 and Kotlin to 2.0.21

---
+++ src/test/kotlin/net/dinkla/nkp/ExampleTest.kt
@@ -0,0 +1,16 @@
+package net.dinkla.nkp
+
+import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.shouldBe
+
+data class Person(val yearOfBirth: Int)
+
+class ExampleTest : StringSpec({
+    "function f should calculate the age of a person" {
+        f(Person(2000)) shouldBe 24
+    }
+})
+
+fun f(person: Person): Int {
+    return 2024 - person.yearOfBirth
+}
