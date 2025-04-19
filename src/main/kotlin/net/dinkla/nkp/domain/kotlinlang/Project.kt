8206874bf8f04765e77f6ded0dfce26be7ce5736
Jörn Dinkla <jdinkla@thoughtworks.com>
2025-04-19 16:46:09 +0200

    refactor packages()

--- src/main/kotlin/net/dinkla/nkp/domain/kotlinlang/Project.kt
+++ src/main/kotlin/net/dinkla/nkp/domain/kotlinlang/Project.kt
@@ -7,16 +7,11 @@ class Project(
     val directory: String,
     val files: List<KotlinFile>,
 ) : List<KotlinFile> by files {
-    fun packages(): List<Package> {
-        val map = mutableMapOf<PackageName, MutableList<KotlinFile>>()
-        for (file in files) {
-            val packageName = file.packageName
-            val list = map.getOrDefault(packageName, mutableListOf())
-            list.add(file)
-            map[packageName] = list
-        }
-        return map.map { Package(it.key, it.value) }.sortedBy { it.packageName.name }
-    }
+    fun packages(): List<Package> =
+        files
+            .groupBy { it.packageName }
+            .map { (name, files) -> Package(name, files) }
+            .sortedBy { it.packageName.name }
 
     fun relativePath(fileName: String): String = fileName.removePrefix(directory).removePrefix("/")
 
